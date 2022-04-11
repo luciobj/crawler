6919fdeba2f8aeaba78a47e4763606f8999080c8
Lúcio BJ <lbjunq@hotmail.com>
2022-04-11 18:52:35 -0300

    feat: added incomplete implementation for top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import get_collection, search_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = get_collection().aggregate([
+            {"$addFields": {
+                "popularidade": {"$add": [
+                    "comments_count",
+                    "shares_count"
+                ]}}},
+            {"$sort": {"popularidade": -1}},
+            {"$limit": 5}])
+    final_result = []
+    for news in result:
+        final_result.append((news["title"], news["url"]))
+    return final_result
 
 
 # Requisito 11
